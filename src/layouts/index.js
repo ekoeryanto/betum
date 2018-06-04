4ff7bbf4df5ae05e93f4e6241e020c81802a6cd8
Eko Eryanto <ekoeryanto@gmail.com>
2018-06-04 07:37:58 +0700

    chore: update title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Betum" />
     <Navbar />
     <div>{children()}</div>
   </div>
